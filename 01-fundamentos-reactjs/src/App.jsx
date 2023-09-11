20b6b750951bd3d200b4fe26c83b72183a7b14b2
DanilloRezende <danillo.rezende3@gmail.com>
2023-09-11 08:04:59 -0300

    first commit

---
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -0,0 +1,36 @@
+import {Post} from './Post';
+import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
+
+import styles from  './App.module.css';
+
+import './global.css';
+
+export function App() {
+
+  return (
+    <>
+      <Header/>
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+          author="Danillo"
+          contet="lorem ipsum dolor sit amet, consectetur adipiscing"
+          />
+          <Post 
+          author="Mariana"
+          contet="lorem ipsum dolor sit amet, consectetur adipiscing"
+          />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          </main>
+      </div>
+    </>
+  )
+}
+
---
+++ 01-fundamentos-reactjs/src/Post.jsx
@@ -0,0 +1,5 @@
+/* eslint-disable react/prop-types */
+export function Post(props) {
+    return <p>{props.author}</p>;
+}
+
